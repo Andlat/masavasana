b69832365e54e3e1f6ac832c2a6b495e0ea62577
Andlat <Andlat@users.noreply.github.com>
2018-10-22 21:21:47 -0400

    Setup live v1.1a
    Remove start of QuillJS img, video and link to prepare for the live version 1.1a

--- js/quill.js
+++ js/quill.js
@@ -21,9 +21,8 @@ window.cQuill = {
       [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
       [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
       /*[{ 'header': [1, 2, 3, 4, 5, 6, false] }],*/
-      [{ 'align': [] }],
-      [ 'link', 'image', 'video', 'formula' ]
-      /*[ 'link', 'image', 'video' ]*/
+      [{ 'align': [] }]/*,
+      [ 'link', 'image', 'video' ]*/
     ];
     let quill = new Quill('#'+id, {
       theme: 'snow',
